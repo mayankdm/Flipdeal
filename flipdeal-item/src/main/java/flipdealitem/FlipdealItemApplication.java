0f7cfbabf098d8108349719990fa65f9ed384f20
mayankdm <mdwivedim@outlook.com>
2023-03-06 17:35:23 +0530

    item service added

---
+++ flipdeal-item/src/main/java/flipdealitem/FlipdealItemApplication.java
@@ -0,0 +1,13 @@
+package flipdealitem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlipdealItemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlipdealItemApplication.class, args);
+	}
+
+}
---
+++ flipdeal-item/src/test/java/flipdealitem/FlipdealItemApplicationTests.java
@@ -0,0 +1,13 @@
+package flipdealitem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlipdealItemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
