99ec15c31530838ff92cec93b03a9c91ca6f0bfd
mayankdm <mdwivedim@outlook.com>
2023-03-06 17:32:56 +0530

    Api added

---
+++ flipdeal-api/src/test/java/flipdealapi/FlipdealApiApplicationTests.java
@@ -0,0 +1,13 @@
+package flipdealapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlipdealApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
